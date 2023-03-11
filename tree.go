826ce5effa6d1620dd0c3fd474d231d5b7aa4c62
Ross Merrigan <email@rossmerrigan.com>
2023-03-11 13:29:31 +0100

    updated insert

--- tree.go
+++ tree.go
@@ -1,203 +1,90 @@
 package radixtree
 
 import (
-	"math"
 	"strings"
 )
 
 type Tree struct {
-	root *node
+	label string
+	edges []*Tree
 }
 
-func NewTree(label string) *Tree {
-	return &Tree{
-		root: newNodeWithLabel(""),
-	}
+func NewRadixTree() *Tree {
+	return &Tree{label: "", edges: []*Tree{}}
 }
 
-func (s *Tree) Insert(label string) {
-	s.insert(label, s.root)
+func (r *Tree) isLeaf() bool {
+	return len(r.edges) == 0
 }
 
-func (s *Tree) insert(label string, currentNode *node) {
-	iterator, newLabel, _, _ := s.enumerate(label, currentNode)
-	inserted := false
-	for iterator.HasNext() {
-		child, _ := iterator.Next()
-		inserted = true
-		s.insert(newLabel, child)
-
-	}
-
-	if !inserted {
-		currentNode.child = append(currentNode.child, newNodeWithLabel(newLabel))
-	}
-
-}
-
-func (s *Tree) Lookup(label string) bool {
-	return s.lookup(label, s.root)
-}
-
-func (s *Tree) lookup(label string, currentNode *node) bool {
-	iterator, newLabel, matches, matched := s.enumerate(label, currentNode)
-
-	for iterator.HasNext() {
-		child, _ := iterator.Next()
-		return s.lookup(newLabel, child)
-
-	}
-	if !matched && matches == len(currentNode.label) {
-		return true
-	}
-
-	return false
-
-}
-
-func (s *Tree) Successor(label string) string {
-	return s.successor(label, s.root, "")
-}
-
-func (s *Tree) successor(label string, currentNode *node, carry string) string {
-	matches := matchingConsecutiveCharacters(label, currentNode)
-
-	if (matches == 0) || (currentNode == s.root) ||
-		((matches > 0) && (matches < len(label))) {
-
-		newLabel := label[matches : len(label)-matches]
-		for _, child := range currentNode.child {
-			if strings.HasPrefix(child.label, string(newLabel[0])) {
-				return s.successor(newLabel, child, carry+currentNode.label)
-			}
-		}
-		return currentNode.label
-	} else if matches < len(currentNode.label) {
-		return carry + currentNode.label
-	} else if matches == len(currentNode.label) {
-		carry = carry + currentNode.label
-
-		min := math.MaxInt
-		index := -1
-		for i := 0; i < len(currentNode.child); i++ {
-			if len(currentNode.child[i].label) < min {
-				min = len(currentNode.child[i].label)
-				index = i
+func (r *Tree) Lookup(x string) bool {
+	traverseNode := r
+	elementsFound := 0
+	for traverseNode != nil && !traverseNode.isLeaf() && elementsFound < len(x) {
+		var nextEdge *Tree
+		// Get the next edge to explore based on the elements not yet found in x
+		//Edge nextEdge := select edge from traverseNode.edges where edge.label is a prefix of x.suffix(elementsFound)
+		for _, edge := range traverseNode.edges {
+			prefix := x[elementsFound:]
+			if strings.HasPrefix(edge.label, prefix) {
+				nextEdge = edge
 			}
 		}
 
-		if index > -1 {
-			return carry + currentNode.child[index].label
+		if nextEdge != nil {
+			traverseNode = nextEdge
+
+			elementsFound += len(nextEdge.label)
 		} else {
-			return carry
+			traverseNode = nil
 		}
 
 	}
-	return ""
 
+	return (traverseNode != nil && traverseNode.isLeaf() && elementsFound == len(x))
 }
 
-func (s *Tree) Predecessor(label string) string {
-	return s.predecessor(label, s.root, "")
-}
-
-func (s *Tree) predecessor(label string, currentNode *node, carry string) string {
-	iterator, newLabel, matches, matched := s.enumerate(label, currentNode)
-
-	for iterator.HasNext() {
-		child, _ := iterator.Next()
-		return s.predecessor(newLabel, child, carry+currentNode.label)
-
-	}
-	if !matched && matches == len(currentNode.label) {
-		return carry + currentNode.label
-	}
-
-	return ""
-}
-
-func (s *Tree) Delete(label string) {
-	s.delete(label, s.root)
-}
-
-func (s *Tree) delete(label string, currentNode *node) {
-	iterator, newLabel, _, _ := s.enumerate(label, currentNode)
-	for iterator.HasNext() {
-		child, index := iterator.Next()
-		if newLabel == child.label {
-			if len(child.child) == 0 {
-				currentNode.child = append(currentNode.child[:index], currentNode.child[index+1:]...)
-				return
+func (r *Tree) Insert(x string) {
+	traverseNode := r
+	elementsFound := 0
+	suffix := ""
+	for traverseNode != nil && !traverseNode.isLeaf() && elementsFound < len(x) {
+		var nextEdge *Tree
+		start := len(x[elementsFound:])
+		end := len(x[:elementsFound])
+		for _, edge := range traverseNode.edges {
+			for i := start; i > end; i-- {
+				suffix = x[elementsFound:i]
+				if strings.HasPrefix(edge.label, suffix) {
+					nextEdge = edge
+					break
+				}
 			}
 		}
-	}
-}
 
-func (s *Tree) enumerate(label string, currentNode *node) (*treeIterator, string, int, bool) {
-	return NewTreeIterator(s, currentNode, label)
-}
-
-type treeIterator struct {
-	currentNode *node
-	indexStart  int
-	indexEnd    int
-	label       string
-	matched     bool
-}
-
-func NewTreeIterator(tree *Tree, currentNode *node, label string) (*treeIterator, string, int, bool) {
-
-	matches := matchingConsecutiveCharacters(label, currentNode)
-
-	newLabel := label[matches : len(label)-matches]
-
-	matched := (matches == 0) || (currentNode == tree.root) ||
-		((matches > 0) && (matches < len(label)) && (matches >= len(currentNode.label)))
-
-	return &treeIterator{
-		indexStart:  0,
-		indexEnd:    len(currentNode.child),
-		currentNode: currentNode,
-		label:       newLabel,
-	}, newLabel, matches, matched
-}
-
-func (s *treeIterator) HasNext() bool {
-	return s.matched && s.indexStart < s.indexEnd
-}
-
-func (s *treeIterator) Next() (child *node, index int) {
-	for index, child = range s.currentNode.child[s.indexStart:] {
-		s.indexStart = index
-		if strings.HasPrefix(child.label, string(s.label[0])) {
-			return
+		if nextEdge != nil {
+			traverseNode = nextEdge
+			elementsFound += len(suffix)
+		} else {
+			break
 		}
-	}
-	return
-}
 
-func matchingConsecutiveCharacters(label string, currentNode *node) int {
-	matches := 0
-	minLength := 0
-
-	if len(currentNode.label) >= len(label) {
-		minLength = len(label)
-	} else if len(currentNode.label) < len(label) {
-		minLength = len(currentNode.label)
 	}
 
-	if minLength > 0 {
-		//go throught the two streams
-		for i := 0; i < minLength; i++ {
-			//if two characters at the same position have the same value we have one more match
-			if label[i] == currentNode.label[i] {
-				matches++
-			} else {
-				//if at any position the two strings have different characters break the cycle
-				break
-			}
-		}
+	if elementsFound == 0 {
+		newLabel := x[elementsFound:]
+		newChild1 := &Tree{label: newLabel}
+
+		traverseNode.edges = append(traverseNode.edges, newChild1)
+	} else {
+		newLabel := traverseNode.label[:len(suffix)]
+		oldLabel1 := traverseNode.label[len(newLabel):]
+		oldLabel2 := x[elementsFound:]
+
+		newChild1 := &Tree{label: oldLabel1, edges: traverseNode.edges}
+		newChild2 := &Tree{label: oldLabel2}
+
+		traverseNode.label = newLabel
+		traverseNode.edges = []*Tree{newChild1, newChild2}
 	}
-	//and return the current number of matches
-	return matches
 }
---
+++ tree_test.go
@@ -0,0 +1,80 @@
+package radixtree
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestNewTree(t *testing.T) {
+	tests := []struct {
+		name   string
+		labels []string
+		want   *Tree
+	}{
+		// {
+		// 	name: "tree",
+		// 	labels: []string{
+		// 		"romane",
+		// 		"romanus",
+		// 		"romulus",
+		// 		"rubens",
+		// 		"ruber",
+		// 		"rubicon",
+		// 		"rubicundus",
+		// 	},
+		// 	want: &Tree{},
+		// },
+		{
+			name: "tree",
+			labels: []string{
+				"PLAN",
+				"PLAY",
+				"POLL",
+				"POST",
+			},
+			want: &Tree{
+				label: "",
+				edges: []*Tree{
+					&Tree{
+						label: "P",
+						edges: []*Tree{
+							&Tree{
+								label: "LA",
+								edges: []*Tree{
+									&Tree{
+										label: "N",
+									},
+									&Tree{
+										label: "Y",
+									},
+								},
+							},
+							&Tree{
+								label: "O",
+								edges: []*Tree{
+									&Tree{
+										label: "LL",
+									},
+									&Tree{
+										label: "ST",
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := NewRadixTree()
+			for _, label := range tt.labels {
+				got.Insert(label)
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewTree() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
